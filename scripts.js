24c9f87d090be87737a70e02210afe1dedfda7b2
Victor Hugo <victorhugo.chdo@gmail.com>
2026-08-24 16:43:40 -0300

    1º primeiro commit

---
+++ scripts.js
@@ -0,0 +1,94 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    //console.log("Funcionou!")
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    if (currencySelect.value == "dolar") {
+        //Se o select estiver selecionando o valor de dolar, entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        //Se o select estiver selecionando o valor de euro, entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+    console.log(inputCurrencyValue)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeCurrency (){
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if(currencySelect.value == "real") {
+        currencyName.innerHTML = "Real brasileiro"
+        currencyImage.src = "./assets/Real.svg"
+    }
+
+    if(currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assets/Dolar.svg"
+    }
+
+    if(currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/Euro.svg"
+    }
+
+    if(currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra esterlina"
+        currencyImage.src = "./assets/Libra.svg"
+    }
+
+    if(currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "BitCoin"
+        currencyImage.src = "./assets/BitCoin.png"
+    }
+
+    convertValues()
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
